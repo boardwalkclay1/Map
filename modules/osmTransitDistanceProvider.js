711d6c556e1a40acea879e3dd3112c08d3553342
boardwalkclay1 <boardwalkclay1@gmail.com>
2026-01-11 14:46:17 -0500

    Create osmTransitDistanceProvider.js

---
+++ modules/osmTransitDistanceProvider.js
@@ -0,0 +1,35 @@
+// modules/osmTransitDistanceProvider.js
+import { OSMPublicTransportProvider } from "./osmPublicTransportProvider.js";
+
+export class OSMTransitDistanceProvider {
+  constructor() {
+    this.transit = new OSMPublicTransportProvider();
+  }
+
+  async nearestTransitStop(bbox, lat, lng) {
+    const stops = await this.transit.getStops(bbox);
+    if (!stops.length) return null;
+
+    let best = null;
+    let bestDist = Infinity;
+
+    for (const s of stops) {
+      const d = this._dist(lat, lng, s.lat, s.lon);
+      if (d < bestDist) {
+        bestDist = d;
+        best = s;
+      }
+    }
+
+    return { stop: best, distance: bestDist };
+  }
+
+  _dist(aLat, aLng, bLat, bLng) {
+    const R = 6371000;
+    const dLat = (bLat - aLat) * Math.PI / 180;
+    const dLng = (bLng - aLng) * Math.PI / 180;
+    const x = dLng * Math.cos((aLat + bLat) / 2 * Math.PI / 180);
+    const y = dLat;
+    return Math.sqrt(x*x + y*y) * R;
+  }
+}
